e76c60948a6d87c532ee84b3ee3c63a6a4568406
maruf aziz <ma'ruf.muzani@students.amikom.ac.id>
2020-07-21 08:00:54 +0700

    x

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -196,7 +196,9 @@ The above copyright notice and this permission notice shall be included in all c
 						</div>
           </li>
           <li class="nav-item active-pro ">
-            <a class="nav-link" href="./upgrade.html">
+            <a class="nav-link" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
               <i class="material-icons">report</i>
               <p>Log Out</p>
             </a>
@@ -220,24 +222,7 @@ The above copyright notice and this permission notice shall be included in all c
             <span class="navbar-toggler-icon icon-bar"></span>
           </button>
           <div class="collapse navbar-collapse justify-content-end">
-            {{-- <form class="navbar-form">
-              <div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                  <i class="material-icons">search</i>
-                  <div class="ripple-container"></div>
-                </button>
-              </div>
-            </form> --}}
             <ul class="navbar-nav">
-              {{-- <li class="nav-item">
-                <a class="nav-link" href="javascript:;">
-                  <i class="material-icons">dashboard</i>
-                  <p class="d-lg-none d-md-block">
-                    Stats
-                  </p>
-                </a>
-              </li> --}}
               <li class="nav-item dropdown">
                 <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="material-icons">notifications</i>
